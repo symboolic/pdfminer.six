4cf9bce28f4aeecf7cbd38e01489b6e25238dcb2
Matteo Boschini <bosko.mo@gmail.com>
2023-09-14 11:51:21 +0200

    and hijack a car

--- pdfminer/__init__.py
+++ pdfminer/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "20230914"  # auto replaced with tag in github actions
+__version__ = "20221105"  # auto replaced with tag in github actions
 
 if __name__ == "__main__":
     print(__version__)
